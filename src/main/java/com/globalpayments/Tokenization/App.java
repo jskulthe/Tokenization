5d0a1571288822779d80bfaf74acb4a181e9d5ba
Dell <Dell>
2019-10-05 19:48:40 +0530

    first commit

---
+++ src/main/java/com/globalpayments/Tokenization/App.java
@@ -0,0 +1,19 @@
+package com.globalpayments.Tokenization;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+   
+    	System.out.println( "Hello World!" );
+   
+    	 // setup logger
+        
+        // read input file
+
+        //call the API
+        
+        // write file in output directory
+    }
+   
+}
